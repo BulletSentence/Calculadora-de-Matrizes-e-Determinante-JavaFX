b297f5aafbc3842e21101188e2d7003c1bd04101
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-22 11:31:14 -0300

    Atualizaão de bibilhotecas e Classes

--- src/AlgebraLinear.java
+++ src/AlgebraLinear.java
@@ -5,7 +5,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AlgebraLinear extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXML/Main.fxml"));
